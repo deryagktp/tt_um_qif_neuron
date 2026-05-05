212c458d5aa79f72d12f2acdc8d3311b97f8eb34
Derya Goktepe <160794871+deryagktp@users.noreply.github.com>
2026-05-04 21:06:55 -0700

    Update test.py

--- test/test.py
+++ test/test.py
@@ -26,9 +26,9 @@ async def test_project(dut):
 
     dut.ui_in.value = 16
 
-    await ClockCycles(dut.clk, 1)
+    await ClockCycles(dut.clk, 2)
     assert dut.uo_out.value.integer == 4
-
+ 
     await ClockCycles(dut.clk, 1)
     assert dut.uo_out.value.integer == 8
 
